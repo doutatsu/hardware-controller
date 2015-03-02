d09973718f7c012693233bdedbbe75c054a0bfd4
doutatsu <doutatsu@me.com>
2015-03-02 20:22:32 +0000

    sync added

--- hardware-controller.py
+++ hardware-controller.py
@@ -11,6 +11,12 @@ def socket_control(device, status):
  else:
    subprocess.call("sudo ./pihat --repeats=10 --id=1 --channel=0 --state=0",shell=True)
  return jsonify(device='lights', status='1')
+@app.route("/send")
+def send_status():
+  print "Sending status"
+  payload = {"device": device, "status": status}
+  r = requests.post("https://192.168.1.69:3000/sync", data=payload)
+  print(r.text)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
