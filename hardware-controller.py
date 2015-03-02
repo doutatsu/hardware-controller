7448ea889957b4e029cf3bce8619a8a12e7073d9
doutatsu <doutatsu@me.com>
2015-03-02 20:38:53 +0000

    update code

--- hardware-controller.py
+++ hardware-controller.py
@@ -12,13 +12,14 @@ app = Flask(__name__)
 def socket_control(device, status):
  if status == "1":
    subprocess.call("sudo ./pihat --repeats=10 --id=1 --channel=0 --state=1",shell=True)
+   return jsonify(device='lights', status='1')
  else:
    subprocess.call("sudo ./pihat --repeats=10 --id=1 --channel=0 --state=0",shell=True)
- return jsonify(device='lights', status='1')
+   return jsonify(device='lights', status='0')
 @app.route("/send")
 def send_status():
   print "Sending status"
-  payload = {"device": device, "status": status}
+  payload = {"device": "Sensor 1", "status": "On"}
   r = requests.post("http://192.168.1.69:3000/sync", data=payload)
   return jsonify(r)
 
