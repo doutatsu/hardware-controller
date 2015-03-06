156b6a31ede4987365adfbc822b5e6758cf824dd
Maxim <doutatsu@me.com>
2015-03-06 21:17:37 +0000

    fix POST request

--- hardware-controller.py
+++ hardware-controller.py
@@ -8,12 +8,12 @@ from time import sleep
 app = Flask(__name__)
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/switch", method='POST')
+@app.route("/switch", methods=['POST'])
 def socket_control():
  if request.form['status'] == "1":
    subprocess.call("cd pihat && sudo ./pihat --repeats=10 --id=1 --channel=0 --state=1",shell=True)
    return jsonify(device='lights', status='1')
- else if request.form['status'] == "0":
+ elif request.form['status'] == "0":
    subprocess.call("cd pihat && sudo ./pihat --repeats=10 --id=1 --channel=0 --state=0",shell=True)
    return jsonify(device='lights', status='0')
 @app.route("/send")
